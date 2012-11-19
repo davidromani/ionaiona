3b3edfd88503c06380272b3b9e485e9ffaa30d65
David Romaní <david@flux.cat>
2012-11-19 20:39:07 +0100

    menus enhacements 1

--- src/IonaIona/PageBundle/Menu/Builder.php
+++ src/IonaIona/PageBundle/Menu/Builder.php
@@ -33,4 +33,73 @@ class Builder extends ContainerAware
 
         return $menu;
     }
+
+    public function main_ca_Menu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setCurrentUri($this->container->get('request')->getRequestUri());
+        $menu->addChild('l\'armari', array('route' => 'armari_ca'));
+        $menu->addChild('el taller', array('route' => 'taller_ca'));
+        $menu->addChild('el diari', array('route' => 'diari_ca'));
+
+        return $menu;
+    }
+
+    public function armari_ca_Menu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('armari');
+        $menu->setCurrentUri($this->container->get('request')->getRequestUri());
+        $menu->addChild('balena', array('route' => 'balena_ca'));
+        $menu->addChild('girafa', array('route' => 'girafa_ca'));
+        $menu->addChild('mico', array('route' => 'mico_ca'));
+        $menu->addChild('gos salsitxa', array('route' => 'gossalsitxa_ca'));
+
+        return $menu;
+    }
+
+    public function main_es_Menu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setCurrentUri($this->container->get('request')->getRequestUri());
+        $menu->addChild('armario', array('route' => 'armari_es'));
+        $menu->addChild('taller', array('route' => 'taller_es'));
+        $menu->addChild('diario', array('route' => 'diari_es'));
+
+        return $menu;
+    }
+
+    public function armari_es_Menu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('armari');
+        $menu->setCurrentUri($this->container->get('request')->getRequestUri());
+        $menu->addChild('ballena', array('route' => 'balena_es'));
+        $menu->addChild('jirafa', array('route' => 'girafa_es'));
+        $menu->addChild('mono', array('route' => 'mico_es'));
+        $menu->addChild('perro salchicha', array('route' => 'gossalsitxa_es'));
+
+        return $menu;
+    }
+
+    public function main_en_Menu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setCurrentUri($this->container->get('request')->getRequestUri());
+        $menu->addChild('wardrobe', array('route' => 'armari_en'));
+        $menu->addChild('workshop', array('route' => 'taller_en'));
+        $menu->addChild('newspaper', array('route' => 'diari_en'));
+
+        return $menu;
+    }
+
+    public function armari_en_Menu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('armari');
+        $menu->setCurrentUri($this->container->get('request')->getRequestUri());
+        $menu->addChild('whale', array('route' => 'balena_en'));
+        $menu->addChild('jiraffe', array('route' => 'girafa_en'));
+        $menu->addChild('monkey', array('route' => 'mico_en'));
+        $menu->addChild('dachshund', array('route' => 'gossalsitxa_en'));
+
+        return $menu;
+    }
 }
