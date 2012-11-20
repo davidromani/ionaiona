3787d9261c6486d223c0c57fca8af94367551eae
David Romaní <david@flux.cat>
2012-11-20 20:22:15 +0100

    homepage update 1

--- src/IonaIona/PageBundle/Controller/DefaultController.php
+++ src/IonaIona/PageBundle/Controller/DefaultController.php
@@ -8,48 +8,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/hello/{name}")
-     * @Template()
-     */
-    public function indexAction($name)
-    {
-        return array('name' => $name);
-    }
-
     public function homeAction()
     {
-        /*$session = $this->getRequest()->getSession();
-        if ($session->has('selectedMenuItem')) {
-            if ($session->get("selectedMenuItem") != "inicio") {
-                $session->set("selectedMenuItem", "inicio");
-            }
-        } else {
-            $session->set("selectedMenuItem", "inicio");
-        }*/
         $em = $this->getDoctrine()->getManager();
         $pagina = $em->getRepository('FluxPageBundle:Page')->findOneBy(array('code' => '001-001'));
-        /*$asociacion = $em->getRepository('ASBAEPageBundle:Page')->findOneBy(array('code' => '001-002'));
-        $junta = $em->getRepository('ASBAEPageBundle:Page')->findOneBy(array('code' => '001-003'));
-        $noticias = $em->getRepository('ASBAEEventsBundle:News')->findLast3ActiveAndOrderedByDate();*/
         return $this->render('PageBundle:Default:home.html.twig', array(
-            //'pagina' => $pagina,
-            //'asociacion' => $asociacion,
-            //'junta' => $junta,
-            //'noticias' => $noticias
+            'pagina' => $pagina,
         ));
     }
 
     public function armariAction()
     {
-        /*$session = $this->getRequest()->getSession();
-        if ($session->has('selectedMenuItem')) {
-            if ($session->get("selectedMenuItem") != "inicio") {
-                $session->set("selectedMenuItem", "inicio");
-            }
-        } else {
-            $session->set("selectedMenuItem", "inicio");
-        }*/
         $em = $this->getDoctrine()->getManager();
         $pagina = $em->getRepository('FluxPageBundle:Page')->findOneBy(array('code' => '001-002'));
         /*$asociacion = $em->getRepository('ASBAEPageBundle:Page')->findOneBy(array('code' => '001-002'));
@@ -65,14 +34,6 @@ class DefaultController extends Controller
 
     public function tallerAction()
     {
-        /*$session = $this->getRequest()->getSession();
-        if ($session->has('selectedMenuItem')) {
-            if ($session->get("selectedMenuItem") != "inicio") {
-                $session->set("selectedMenuItem", "inicio");
-            }
-        } else {
-            $session->set("selectedMenuItem", "inicio");
-        }*/
         $em = $this->getDoctrine()->getManager();
         $pagina = $em->getRepository('FluxPageBundle:Page')->findOneBy(array('code' => '001-003'));
         /*$asociacion = $em->getRepository('ASBAEPageBundle:Page')->findOneBy(array('code' => '001-002'));
@@ -88,14 +49,6 @@ class DefaultController extends Controller
 
     public function diariAction()
     {
-        /*$session = $this->getRequest()->getSession();
-        if ($session->has('selectedMenuItem')) {
-            if ($session->get("selectedMenuItem") != "inicio") {
-                $session->set("selectedMenuItem", "inicio");
-            }
-        } else {
-            $session->set("selectedMenuItem", "inicio");
-        }*/
         $em = $this->getDoctrine()->getManager();
         $pagina = $em->getRepository('FluxPageBundle:Page')->findOneBy(array('code' => '001-004'));
         /*$asociacion = $em->getRepository('ASBAEPageBundle:Page')->findOneBy(array('code' => '001-002'));
