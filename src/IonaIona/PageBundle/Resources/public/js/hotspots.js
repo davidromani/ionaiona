791a97422b7303df99edb4881c1a18cae0a66ed0
David Romaní <david@flux.cat>
2012-11-25 21:16:24 +0100

    Wardrobe JS animation 2

--- src/IonaIona/PageBundle/Resources/public/js/hotspots.js
+++ src/IonaIona/PageBundle/Resources/public/js/hotspots.js
@@ -1,100 +1,68 @@
 var APP = [];
-APP.sinusCounter = 0;
-APP.MAX_NINOTS_TO_ANIMATE = 3;
-APP.NINOTS_ARRAY = [];
 
-// Este codigo se ejecuta immediatamente despues de que
-// el navegador termine de cargar el DOM
-jQuery(document).ready(function($) {
-
-    // Calcula el tiempo que va a tardar disparar el temporizador inicial
+// Evento definido para asignar un temporizador aleatorio sobre 
+// un ninotID pasado por parametro
+jQuery(document).on('app/setMovement', function(evt, param) {
     var r = getRandomInt(1500, 5500);
-    console.log('[ready] r = ' + r);
-    APP.fireInterval = setTimeout("fireInterval()", r);
-
-    // Calcula la matriz de ninos a mover
-    fillArrayOfNinotsToAnimate();
-
-/*    $('#hotspot').click(function(evt) {
-        $(document).trigger('app/showPanel', 'first');
-        $('#hotspot').unbind('mouseout');
-    });
-
-    // Asigna la funcion que se va a disparar al situar el puntero sobre #hotsopt
-    $('#hotspot').mouseover(function(evt) {
-        $('#dark-background-layer').fadeTo(250, 1, 'swing');
-        blinkOff();
-    });
-
-    // Asigna la funcion que se va a disparar al situar el puntero fuera de #hotsopt
-    $('#hotspot').mouseout(function(evt) {
-        $('#dark-background-layer').fadeTo(250, 0, 'swing');
-        blinkOn();
-    });
-
-    // Asigna la funcion que animara la intensidad de la capa de iluminacion continuamente
-    blinkOn();
-*/
+    console.log('[app/setMovement] fired param=' + param + ' with timeout=' + r);   
+    setTimeout(fireInterval, r, param);
 });
 
-function fillArrayOfNinotsToAnimate() {
-    console.log('[fillArrayOfNinotsToAnimate] hit!');
-    if (APP.NINOTS_ARRAY.length == 0) {
-        console.log('[fillArrayOfNinotsToAnimate] len == 0');
-        var r = getRandomInt(1, APP.MAX_NINOTS_TO_ANIMATE);
-        APP.NINOTS_ARRAY.push(r);
-        var r2 = getRandomInt(1, APP.MAX_NINOTS_TO_ANIMATE);
-        while (r == r2) {
-            r2 = getRandomInt(1, APP.MAX_NINOTS_TO_ANIMATE);
-        }
-        APP.NINOTS_ARRAY.push(r2);
+// Paso de animacion 1: movimento hacia la izquierda
+function fireInterval(param) {
+    console.log('[fireInterval] hit! param=' + param);
+    var n = $('#' + param.id);
+    var t = "rotate(-1 " + (param.x + param.width/2) + " " + (param.y + param.height/2) + ")";
+    console.log('[fireInterval] transform=' + t);
+    n.attr("transform", t);
+    setTimeout(fireInterval2, 250, param);
+}
+
+// Paso de animacion 2: movimento hacia la derecha
+function fireInterval2(param) {
+    console.log('[fireInterval2] hit! param=' + param);
+    var n = $('#' + param.id);
+    var t = "rotate(1 " + (param.x + param.width/2) + " " + (param.y + param.height/2) + ")";
+    console.log('[fireInterval2] transform=' + t);
+    n.attr("transform", t);
+    setTimeout(fireInterval3, 250, param);
+}
+
+// Paso de animacion 3: movimento hacia la izquierda
+function fireInterval3(param) {
+    console.log('[fireInterval3] hit! param=' + param);
+    var n = $('#' + param.id);
+    var t = "rotate(-2 " + (param.x + param.width/2) + " " + (param.y + param.height/2) + ")";
+    console.log('[fireInterval3] transform=' + t);
+    n.attr("transform", t);
+    setTimeout(fireInterval4, 250, param);
+}
+
+// Paso de animacion 4: movimento hacia la derecha
+function fireInterval4(param) {
+    console.log('[fireInterval4] hit! param=' + param);
+    var n = $('#' + param.id);
+    var t = "rotate(2 " + (param.x + param.width/2) + " " + (param.y + param.height/2) + ")";
+    console.log('[fireInterval4] transform=' + t);
+    n.attr("transform", t);
+    setTimeout(fireIntervalFinal, 250, param);
+}
+
+// Paso de animacion final: movimento hacia el centro y reasignacion del temporizador aleatorio
+function fireIntervalFinal(param) {
+    console.log('[fireIntervalFinal] hit! param=' + param);
+    var n = $('#' + param.id);
+    var t = "rotate(0 " + (param.x + param.width/2) + " " + (param.y + param.height/2) + ")";
+    console.log('[fireIntervalFinal] transform=' + t);
+    n.attr("transform", t);
+    if (param.id == 'ninot_balena') {
+        $(document).trigger('app/setMovement', APP.balena);
+    } else if (param.id == 'ninot_girafa') {
+        $(document).trigger('app/setMovement', APP.girafa);
+    } else if (param.id == 'ninot_ocell') {
+        $(document).trigger('app/setMovement', APP.ocell);
     }
 }
-
-// Evento definido para mostrar el panel inferior
-jQuery(document).on('app/showPanel', function(evt, param) {
-    //console.log('[app/showPanel] fired param=' + param);
-    $('#hidden-bottom-panel').animate({
-        'top' : '-1100px'
-    }, { duration : 'slow' });
-    $('#hidden-bottom-panel').click(function(evt) {
-        $(document).trigger('app/hidePanel', 'second');
-    });
-});
-
-// Evento definido para ocultar el panel inferior
-jQuery(document).on('app/hidePanel', function(evt, param) {
-    //console.log('[app/hidePanel] fired param=' + param);
-    $('#dark-background-layer').fadeTo(250, 0, 'swing');
-    $('#hidden-bottom-panel').animate({
-        'top' : '-836px'
-    }, { duration : 'slow' });
-    $('#hotspot').mouseout(function(evt) {
-        $('#dark-background-layer').fadeTo(250, 0, 'swing');
-        blinkOn();
-    });
-    $('#hidden-bottom-panel').unbind('click');
-    blinkOn();
-});
-
-function fireInterval() {
-    //console.log('[fireInterval] hit!');
-    var r = getRandomInt(1500, 5500);
-    //console.log('[fireInterval] new r = ' + r);
-    APP.fireInterval = setTimeout("fireInterval()", r);
-}
-
-function blinkOff() {
-    clearInterval(APP.blinkInterval);
-}
-
-function fadeSinusInOut(id) {
-    APP.sinusCounter = (APP.sinusCounter + 1) % 40;
-    var opv = Math.round(((Math.sin(0.05 * Math.PI * APP.sinusCounter) + 1)) / 2 * 100 ) / 100;
-    //console.log('opacity val = ' + opv);
-    $(id).css('opacity', opv);
-}
-
 /**
  * Returns a random integer between min and max
  * Using Math.round() will give you a non-uniform distribution!
