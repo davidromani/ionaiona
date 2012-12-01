6d430249b5553c49bd88e7ec5c5a98ff08be4f86
David Romaní <david@flux.cat>
2012-12-01 18:50:00 +0100

    Restructure 6

--- src/IonaIona/PageBundle/DataFixtures/ORM/Pages.php
+++ src/IonaIona/PageBundle/DataFixtures/ORM/Pages.php
@@ -64,6 +64,136 @@ class Pages implements FixtureInterface
         $page->addTranslation($translation);
 
         $manager->persist($page);
+
+        $page = new Page();
+        $page->setCode('002-001');
+        $page->setTitle('història');
+        $page->setSummary('història...');
+        $page->setText1("La història de iona iona i la balena. Hi havia una vegada...<br>Adaptació de la història bíblica<br>(...)<br>");
+        $page->setImage1("history.png");
+        $page->setPosition(1);
+        $page->setIsActive(true);
+
+        $translation = new PageTranslation();
+        $translation->setLocale('en');
+        $translation->setField('title');
+        $translation->setContent('history');
+        $page->addTranslation($translation);
+        $translation = new PageTranslation();
+        $translation->setLocale('en');
+        $translation->setField('summary');
+        $translation->setContent('history...');
+        $page->addTranslation($translation);
+        $translation = new PageTranslation();
+        $translation->setLocale('en');
+        $translation->setField('text1');
+        $translation->setContent("The history of Iona Iona and the whale. Once upon a time...<br>Adaptation of the biblical story<br>(...)<br>");
+        $page->addTranslation($translation);
+
+        $translation = new PageTranslation();
+        $translation->setLocale('es');
+        $translation->setField('title');
+        $translation->setContent('historia');
+        $page->addTranslation($translation);
+        $translation = new PageTranslation();
+        $translation->setLocale('es');
+        $translation->setField('summary');
+        $translation->setContent('historia...');
+        $page->addTranslation($translation);
+        $translation = new PageTranslation();
+        $translation->setLocale('es');
+        $translation->setField('text1');
+        $translation->setContent("La historia de Iona Iona y la ballena. Había una vez...<br>Adaptación de la historia bíblica<br>(...)<br>");
+        $page->addTranslation($translation);
+
+        $manager->persist($page);
+
+        $page = new Page();
+        $page->setCode('002-002');
+        $page->setTitle('artesania');
+        $page->setSummary('artesania...');
+        $page->setText1("Explicar el procés d'elaboració del producte, des de com es treu el patró, elecció de teles, com es talla, com es confecciona i com s'omple.<br>(...)<br>");
+        $page->setImage1("crafts.png");
+        $page->setPosition(2);
+        $page->setIsActive(true);
+
+        $translation = new PageTranslation();
+        $translation->setLocale('en');
+        $translation->setField('title');
+        $translation->setContent('crafts');
+        $page->addTranslation($translation);
+        $translation = new PageTranslation();
+        $translation->setLocale('en');
+        $translation->setField('summary');
+        $translation->setContent('crafts...');
+        $page->addTranslation($translation);
+        $translation = new PageTranslation();
+        $translation->setLocale('en');
+        $translation->setField('text1');
+        $translation->setContent("Explain the process of developing the product, from how to remove the pattern, choice of fabrics, cuts and as such is made and filled.<br>(...)<br>");
+        $page->addTranslation($translation);
+
+        $translation = new PageTranslation();
+        $translation->setLocale('es');
+        $translation->setField('title');
+        $translation->setContent('artesania');
+        $page->addTranslation($translation);
+        $translation = new PageTranslation();
+        $translation->setLocale('es');
+        $translation->setField('summary');
+        $translation->setContent('artesania...');
+        $page->addTranslation($translation);
+        $translation = new PageTranslation();
+        $translation->setLocale('es');
+        $translation->setField('text1');
+        $translation->setContent("Explicar el proceso de elaboración del producto, desde cómo se saca el patrón, elección de telas, como se corta, como se confecciona y como se llena.<br>(...)<br>");
+        $page->addTranslation($translation);
+
+        $manager->persist($page);
+
+        $page = new Page();
+        $page->setCode('002-003');
+        $page->setTitle('proximitat');
+        $page->setSummary('proximitat...');
+        $page->setText1("Proximitat.<br>(...)<br>");
+        $page->setImage1("proximity.png");
+        $page->setPosition(3);
+        $page->setIsActive(true);
+
+        $translation = new PageTranslation();
+        $translation->setLocale('en');
+        $translation->setField('title');
+        $translation->setContent('crafts');
+        $page->addTranslation($translation);
+        $translation = new PageTranslation();
+        $translation->setLocale('en');
+        $translation->setField('summary');
+        $translation->setContent('proxiity...');
+        $page->addTranslation($translation);
+        $translation = new PageTranslation();
+        $translation->setLocale('en');
+        $translation->setField('text1');
+        $translation->setContent("Proxiity.<br>(...)<br>");
+        $page->addTranslation($translation);
+
+        $translation = new PageTranslation();
+        $translation->setLocale('es');
+        $translation->setField('title');
+        $translation->setContent('proximidad');
+        $page->addTranslation($translation);
+        $translation = new PageTranslation();
+        $translation->setLocale('es');
+        $translation->setField('summary');
+        $translation->setContent('proximidad...');
+        $page->addTranslation($translation);
+        $translation = new PageTranslation();
+        $translation->setLocale('es');
+        $translation->setField('text1');
+        $translation->setContent("Proximidad.<br>(...)<br>");
+        $page->addTranslation($translation);
+
+        $manager->persist($page);
+
         $manager->flush();
     }
 }
