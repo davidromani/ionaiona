1947896be53daf9ef28bdd72bdc5c86a5cf3b62e
David Romaní <david@flux.cat>
2013-01-10 19:33:22 +0100

    xml sitemap added v0.2

--- src/IonaIona/PageBundle/Controller/SitemapController.php
+++ src/IonaIona/PageBundle/Controller/SitemapController.php
@@ -19,6 +19,43 @@ class SitemapController extends Controller
 
         array_push($urls, array('loc' => $this->get('router')->generate('inicial'), 'changefreq' => 'weekly', 'priority' => '1.0'));
 
+
+
+        // CA
+        array_push($urls, array('loc' => $this->get('router')->generate('home_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '1.0'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_balena_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_girafa_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_gossalsitxa_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_pitet_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_ocell_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_banderola_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_mobil_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        foreach ($em->getRepository('FluxProductBundle:Product')->getSortedActiveItems() as $item) {
+            array_push($urls, array('loc' => $this->get('router')->generate('armari_items_ca',
+                array('_locale' => 'ca',
+                    'nom' => $item->getSlug(),
+                    'id' => $item->getId())), 'changefreq' => 'weekly', 'priority' => '0.3'));
+        }
+        array_push($urls, array('loc' => $this->get('router')->generate('taller_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('taller_historia_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('taller_artesania_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('taller_proximitat_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('diari_ca', array('_locale' => 'ca')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        foreach ($em->getRepository('FluxBlogBundle:Post')->getAllPostsSortedByDateQuery()->getResult() as $item) {
+            array_push($urls, array('loc' => $this->get('router')->generate('diari_post_ca',
+                array('_locale' => 'ca',
+                    'year' => $item->getPostDate()->format('Y'),
+                    'month' => $item->getPostDate()->format('m'),
+                    'day' => $item->getPostDate()->format('d'),
+                    'title' => $item->getTitleSlug(),
+                    'id' => $item->getId())), 'changefreq' => 'weekly', 'priority' => '0.3'));
+        }
+
+
+
+
+
         // ES
         array_push($urls, array('loc' => $this->get('router')->generate('home_es', array('_locale' => 'es')), 'changefreq' => 'weekly', 'priority' => '1.0'));
         array_push($urls, array('loc' => $this->get('router')->generate('armari_es', array('_locale' => 'es')), 'changefreq' => 'weekly', 'priority' => '0.5'));
@@ -29,34 +66,61 @@ class SitemapController extends Controller
         array_push($urls, array('loc' => $this->get('router')->generate('armari_ocell_es', array('_locale' => 'es')), 'changefreq' => 'weekly', 'priority' => '0.5'));
         array_push($urls, array('loc' => $this->get('router')->generate('armari_banderola_es', array('_locale' => 'es')), 'changefreq' => 'weekly', 'priority' => '0.5'));
         array_push($urls, array('loc' => $this->get('router')->generate('armari_mobil_es', array('_locale' => 'es')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        foreach ($em->getRepository('FluxProductBundle:Product')->getSortedActiveItems() as $item) {
+            array_push($urls, array('loc' => $this->get('router')->generate('armari_items_es',
+                array('_locale' => 'es',
+                    'nom' => $item->getSlug(),
+                    'id' => $item->getId())), 'changefreq' => 'weekly', 'priority' => '0.3'));
+        }
         array_push($urls, array('loc' => $this->get('router')->generate('taller_es', array('_locale' => 'es')), 'changefreq' => 'weekly', 'priority' => '0.5'));
         array_push($urls, array('loc' => $this->get('router')->generate('taller_historia_es', array('_locale' => 'es')), 'changefreq' => 'weekly', 'priority' => '0.5'));
         array_push($urls, array('loc' => $this->get('router')->generate('taller_artesania_es', array('_locale' => 'es')), 'changefreq' => 'weekly', 'priority' => '0.5'));
         array_push($urls, array('loc' => $this->get('router')->generate('taller_proximitat_es', array('_locale' => 'es')), 'changefreq' => 'weekly', 'priority' => '0.5'));
         array_push($urls, array('loc' => $this->get('router')->generate('diari_es', array('_locale' => 'es')), 'changefreq' => 'weekly', 'priority' => '0.5'));
-        /*foreach ($em->getRepository('ASBAEServicesBundle:GroupService')->findAllActiveAndOrderedByPosition() as $group) {
-            foreach ($group->getServices() as $service) {
-                array_push($urls, array('loc' => $this->get('router')->generate('detalleservicio_es',
-                    array('_locale' => 'es',
-                        'grupo' => $group->slug(),
-                        'gid' => $group->getId(),
-                        'servicio' => $service->slug(),
-                        'sid' => $service->getId())), 'changefreq' => 'weekly', 'priority' => '0.5'));
-            }
-        }*/
-
-        /* multi-lang pages
-        foreach($languages as $lang) {
-            $urls[] = array('loc' => $this->get('router')->generate('home_contact', array('_locale' => $lang)), 'changefreq' => 'monthly', 'priority' => '0.3');
+        foreach ($em->getRepository('FluxBlogBundle:Post')->getAllPostsSortedByDateQuery()->getResult() as $item) {
+            array_push($urls, array('loc' => $this->get('router')->generate('diari_post_es',
+                array('_locale' => 'es',
+                    'year' => $item->getPostDate()->format('Y'),
+                    'month' => $item->getPostDate()->format('m'),
+                    'day' => $item->getPostDate()->format('d'),
+                    'title' => $item->getTitleSlug(),
+                    'id' => $item->getId())), 'changefreq' => 'weekly', 'priority' => '0.3'));
         }
 
-        // urls from database
-        $urls[] = array('loc' => $this->get('router')->generate('home_product_overview', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.7');
-        // service
-        foreach ($em->getRepository('AcmeSampleStoreBundle:Product')->findAll() as $product) {
-            $urls[] = array('loc' => $this->get('router')->generate('home_product_detail',
-                array('productSlug' => $product->getSlug())), 'priority' => '0.5');
-        }*/
+
+
+
+
+        // EN
+        array_push($urls, array('loc' => $this->get('router')->generate('home_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '1.0'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_balena_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_girafa_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_gossalsitxa_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_pitet_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_ocell_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_banderola_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('armari_mobil_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        foreach ($em->getRepository('FluxProductBundle:Product')->getSortedActiveItems() as $item) {
+            array_push($urls, array('loc' => $this->get('router')->generate('armari_items_en',
+                array('_locale' => 'en',
+                    'nom' => $item->getSlug(),
+                    'id' => $item->getId())), 'changefreq' => 'weekly', 'priority' => '0.3'));
+        }
+        array_push($urls, array('loc' => $this->get('router')->generate('taller_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('taller_historia_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('taller_artesania_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('taller_proximitat_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        array_push($urls, array('loc' => $this->get('router')->generate('diari_en', array('_locale' => 'en')), 'changefreq' => 'weekly', 'priority' => '0.5'));
+        foreach ($em->getRepository('FluxBlogBundle:Post')->getAllPostsSortedByDateQuery()->getResult() as $item) {
+            array_push($urls, array('loc' => $this->get('router')->generate('diari_post_en',
+                array('_locale' => 'en',
+                    'year' => $item->getPostDate()->format('Y'),
+                    'month' => $item->getPostDate()->format('m'),
+                    'day' => $item->getPostDate()->format('d'),
+                    'title' => $item->getTitleSlug(),
+                    'id' => $item->getId())), 'changefreq' => 'weekly', 'priority' => '0.3'));
+        }
 
         return array('urls' => $urls, 'hostname' => $hostname);
     }
