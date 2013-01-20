6544a75aa236453217310905c8f3aef04f9fbdf9
David Romaní <david@flux.cat>
2013-01-20 23:40:56 +0100

    first approach to the observer design pattern

--- src/IonaIona/PageBundle/Resources/public/js/hotspots.js
+++ src/IonaIona/PageBundle/Resources/public/js/hotspots.js
@@ -1,11 +1,53 @@
 var APP = [];
 
+subject = {
+
+    queue: [],
+    observers: [],
+    needsReloadQueue : true,
+
+    registerObserver: function(observer) {
+        this.observers.push(observer);
+    },
+
+    removeObserver: function(observer) {
+        var i = this.observers.indexOf(observer);
+        if (i >= 0) this.observers.splice(i, 1);
+    },
+
+    notifyObservers: function() {
+        for (var i = 0; i < this.observers.length; i++) {
+            this.observers[i].update();
+        }
+    },
+
+    notifyObserverAtIndex: function(index) {
+        this.observers[index].update();
+    },
+
+    init: function() {
+        //console.log('I have ' + this.observers.length + ' observers');
+        //console.log('I go to choose a random observer');
+        var selected = getRandomInt(0, subject.observers.length-1);
+        console.log('Selected observer index = ' + selected);
+        subject.notifyObserverAtIndex(selected);
+    },
+
+    reloadQueue: function() {
+        for (var i = 0; i < this.observers.length; i++) {
+            var selected = getRandomInt(0, this.observers.length-1);
+
+        }
+    }
+}
+
 // Evento definido para asignar un temporizador aleatorio sobre 
 // un ninotID pasado por parametro
-jQuery(document).on('app/setMovement', function(evt, param) {
+jQuery(document).on('app/setMovement', function(evt) {
     var r = getRandomInt(1500, 5500);
-    //console.log('[app/setMovement] fired param=' + param + ' with timeout=' + r);   
-    setTimeout(fireInterval, r, param);
+    console.log('[app/setMovement] fired with timeout = ' + r);   
+    //setTimeout(fireInterval, r, param);
+    setTimeout(subject.init, r);
 });
 
 // Paso de animacion 1: movimento hacia la izquierda
@@ -55,7 +97,8 @@ function fireIntervalFinal(param) {
     var t = "rotate(0 " + (param.x + param.width/2) + " " + (param.y + param.height/2) + ")";
     //console.log('[fireIntervalFinal] transform=' + t);
     n.attr("transform", t);
-    if (param.id == 'ninot_balena') {
+    $(document).trigger('app/setMovement');
+    /*if (param.id == 'ninot_balena') {
         $(document).trigger('app/setMovement', APP.balena);
     } else if (param.id == 'ninot_girafa') {
         $(document).trigger('app/setMovement', APP.girafa);
@@ -65,7 +108,7 @@ function fireIntervalFinal(param) {
         $(document).trigger('app/setMovement', APP.pitet);
     } else if (param.id == 'ninot_gossalsitxa') {
         $(document).trigger('app/setMovement', APP.gos);
-    }
+    }*/
 }
 /**
  * Returns a random integer between min and max
