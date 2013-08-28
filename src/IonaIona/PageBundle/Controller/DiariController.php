a8d2ad398b05935e105d8d93cc1d7dc476b5e0aa
David Romaní <david@flux.cat>
2013-08-28 10:53:42 +0200

    step 2.1 - blog category search results improved

--- src/IonaIona/PageBundle/Controller/DiariController.php
+++ src/IonaIona/PageBundle/Controller/DiariController.php
@@ -18,6 +18,7 @@ class DiariController extends Controller
         $categories = $em->getRepository('FluxBlogBundle:Category')->getActiveItemsSortedByTitle();
         $post = $em->getRepository('FluxBlogBundle:Post')->find($id);
         $archives = $em->getRepository('FluxBlogBundle:Post')->getArrayOfArchives();
+
         return $this->render('PageBundle:Diari:post.html.twig', array(
             'categories' => $categories,
             'post' => $post,
@@ -34,6 +35,7 @@ class DiariController extends Controller
         $paginator = $this->get('knp_paginator');
         $posts = $paginator->paginate($postsQuery, $this->getRequest()->query->get('page', 1), 10 /*limit per page*/);
         $archives = $em->getRepository('FluxBlogBundle:Post')->getArrayOfArchives();
+
         return $this->render('PageBundle:Diari:arxiu.html.twig', array(
             'pagina' => $pagina,
             'categories' => $categories,
@@ -49,21 +51,17 @@ class DiariController extends Controller
         $em = $this->getDoctrine()->getManager();
         $pagina = $em->getRepository('FluxPageBundle:Page')->findOneBy(array('code' => '001-004'));
         $categories = $em->getRepository('FluxBlogBundle:Category')->getActiveItemsSortedByTitle();
-        $postsQuery = $em->getRepository('FluxBlogBundle:Post')->getPostsFromCategoryIdSortedByDateQuery($id);
+        /** @var Category $selectedCategory */
+        $selectedCategory = $em->getRepository('FluxBlogBundle:Category')->find($id);
+        $posts = $selectedCategory->getPosts();
         //$paginator = $this->get('knp_paginator');
         //$posts = $paginator->paginate($postsQuery, $this->getRequest()->query->get('page', 1), 10 /*limit per page*/);
         $archives = $em->getRepository('FluxBlogBundle:Post')->getArrayOfArchives();
 
-        $logger = $this->get('logger');
-        $logger->debug('Hit !! '.$postsQuery.' count='.count($postsQuery));
-        foreach ($postsQuery as $post) {
-            $logger->debug($post);
-        }
-
         return $this->render('PageBundle:Diari:categories.html.twig', array(
             'pagina' => $pagina,
             'categories' => $categories,
-            'posts' => $postsQuery, //$posts,
+            'posts' => $posts, //$postsQuery, //$posts,
             'archives' => $archives,
             'cid' => $id
         ));
